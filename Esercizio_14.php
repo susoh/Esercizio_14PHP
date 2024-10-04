a2c9f6144c6875c4ac68f9e2bce3e5ad321d7622
Davide <168528917+susoh@users.noreply.github.com>
2024-10-04 09:03:52 +0200

    Update Esercizio_14.php

--- Esercizio_14.php
+++ Esercizio_14.php
@@ -6,6 +6,23 @@
     <title>Document</title>
 </head>
 <body>
+    <?php 
+
+    function generaSfondo($lar) {
+        if($lar < 150) {
+            return "#7FFFD4";
+        } else if($lar >= 150 && $lar <= 250) {
+            return "#1E90FF";
+        } else {
+            return "#0000FF";
+        }
+    }
     
-</body>
+    $larghezza = rand(50, 500);
+    $colore = generaSfondo($l);
+    $altezza = $larghezza * 2;
+
+    echo "<div style='width: {$l}px; height: {$altezza}px; background-color: $colore'></div>";
+    
+    ?></body>
 </html>
